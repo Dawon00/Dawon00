ca06fb3d82cebadd91a598b5bc787bbbcbeea072
Dawon00 <dawon000203@gmail.com>
2024-05-27 17:11:04 +0900

    chore: Update latest blog posts section in README.md

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -18,16 +18,16 @@ const parser = new Parser({
   const feed = await parser.parseURL("https://dawonny.tistory.com/rss");
 
   // 최신 5개의 글의 제목과 링크를 추가할 텍스트 생성
-  let latestPosts = "\n### 📕 Latest Blog Posts\n\n";
+  let latestPosts = "\n### Latest Blog Posts\n\n";
   for (let i = 0; i < 5 && i < feed.items.length; i++) {
     const { title, link } = feed.items[i];
     latestPosts += `- [${title}](${link})\n`;
   }
 
   // 기존 README.md에 최신 블로그 포스트 추가
-  if (readmeContent.includes("### 📕 Latest Blog Posts")) {
+  if (readmeContent.includes("### Latest Blog Posts")) {
     readmeContent = readmeContent.replace(
-      /### 📕 Latest Blog Posts[\s\S]*?(?=\n\n## |\n$)/,
+      /### Latest Blog Posts[\s\S]*?(?=\n\n## |\n$)/,
       latestPosts
     );
   } else {
